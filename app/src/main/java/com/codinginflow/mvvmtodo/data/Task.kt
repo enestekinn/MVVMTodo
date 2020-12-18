df9d7551adb4b415ee8c0655eb508600cd142c28
Enes Tekin <>
2020-12-19 00:47:15 +0300

    XML - Task class

--- app/src/main/java/com/codinginflow/mvvmtodo/data/Task.kt
+++ app/src/main/java/com/codinginflow/mvvmtodo/data/Task.kt
@@ -6,15 +6,18 @@ import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 import java.text.DateFormat
 
-@Entity(tableName = "task_table")
+
+@Entity(tableName = "task_table") // koymazsak class in ismini kullanir.
 @Parcelize
 data class Task(
-    val name: String,
-    val important: Boolean = false,
-    val completed: Boolean = false,
-    val created: Long = System.currentTimeMillis(),
-    @PrimaryKey(autoGenerate = true) val id: Int = 0
+    val name : String,
+    val important : Boolean = false,
+    val completed : Boolean = false,
+    val created : Long = System.currentTimeMillis(),
+
+    @PrimaryKey(autoGenerate = true)
+    val  id : Int = 0
 ) : Parcelable {
-    val createdDateFormatted: String
-        get() = DateFormat.getDateTimeInstance().format(created)
+    val createdDateFormatted : String
+    get() = DateFormat.getTimeInstance().format(created)
 }
